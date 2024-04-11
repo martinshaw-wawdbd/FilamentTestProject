f45145b2324cbb4cf02813d0b3d0f547f64e2cd1
Martin Shaw <martin.shaw@wealthatwork.co.uk>
2024-04-11 10:48:57 +0100

    Add money cast, add Treatments relation manager

--- app/Filament/Resources/PatientResource/RelationManagers/TreatmentsRelationManager.php
+++ app/Filament/Resources/PatientResource/RelationManagers/TreatmentsRelationManager.php
@@ -20,7 +20,15 @@ class TreatmentsRelationManager extends RelationManager
             ->schema([
                 Forms\Components\TextInput::make('description')
                     ->required()
-                    ->maxLength(255),
+                    ->maxLength(255)
+                    ->columnSpan('full'),
+                Forms\Components\Textarea::make('notes')
+                    ->maxLength(65535)
+                    ->columnSpan('full'),
+                Forms\Components\TextInput::make('price')
+                    ->numeric()
+                    ->prefix('€')
+                    ->maxValue(42949672.95),
             ]);
     }
 
@@ -30,6 +38,11 @@ class TreatmentsRelationManager extends RelationManager
             ->recordTitleAttribute('description')
             ->columns([
                 Tables\Columns\TextColumn::make('description'),
+                Tables\Columns\TextColumn::make('price')
+                    ->money('EUR')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime(),
             ])
             ->filters([
                 //
